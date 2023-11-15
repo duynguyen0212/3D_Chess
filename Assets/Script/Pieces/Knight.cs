9f707137c448e072ef20419ebc67260aff9c4999
duynguyen0212 <ngocduy.nguyen0212@gmail.com>
2023-11-15 12:49:43 -0500

    available moves for remaining chess piece

--- Assets/Script/Pieces/Knight.cs
+++ Assets/Script/Pieces/Knight.cs
@@ -4,7 +4,65 @@ using UnityEngine;
 
 public class Knight : ChessPiece
 {
-    // Start is called before the first frame update
+
+    public override List<Vector2Int> GetAvailableMove(ref ChessPiece [,] board, int tileCountX, int tileCountY)
+    {
+        List<Vector2Int> r = new List<Vector2Int>();
+
+        // Top right
+        int x = currentX + 1;
+        int y = currentY + 2;
+        if(x <tileCountX && y < tileCountY)
+            if(board[x, y] == null || board[x, y].team != team)
+                r.Add(new Vector2Int(x,y));
+
+        x = currentX + 2;
+        y = currentY + 1;
+        if(x <tileCountX && y < tileCountY)
+            if(board[x, y] == null || board[x, y].team != team)
+                r.Add(new Vector2Int(x,y));
+
+         // Top left
+        x = currentX - 1;
+        y = currentY + 2;
+        if(x >= 0 && y < tileCountY)
+            if(board[x, y] == null || board[x, y].team != team)
+                r.Add(new Vector2Int(x,y));
+        
+        x = currentX - 2;
+        y = currentY + 1;
+        if(x >= 0 && y < tileCountY)
+            if(board[x, y] == null || board[x, y].team != team)
+                r.Add(new Vector2Int(x,y));
+        
+        // bottom right
+        x = currentX + 1;
+        y = currentY - 2;
+        if(x < tileCountX && y >= 0)
+            if(board[x, y] == null || board[x, y].team != team)
+                r.Add(new Vector2Int(x,y));
+
+        x = currentX + 2;
+        y = currentY - 1;
+        if(x < tileCountX && y >= 0)
+            if(board[x, y] == null || board[x, y].team != team)
+                r.Add(new Vector2Int(x,y));
+
+        // bottom left
+        x = currentX - 1;
+        y = currentY - 2;
+        if(x >= 0 && y >= 0)
+            if(board[x, y] == null || board[x, y].team != team)
+                r.Add(new Vector2Int(x,y));
+
+        x = currentX - 2;
+        y = currentY - 1;
+        if(x >= 0 && y >= 0)
+            if(board[x, y] == null || board[x, y].team != team)
+                r.Add(new Vector2Int(x,y));
+
+        return r;
+    }
     void Start()
     {
         
--- Assets/Script/Pieces/Rook.cs
+++ Assets/Script/Pieces/Rook.cs
@@ -5,6 +5,67 @@ using UnityEngine;
 public class Rook : ChessPiece
 {
     // Start is called before the first frame update
+    public override List<Vector2Int> GetAvailableMove(ref ChessPiece [,] board, int tileCountX, int tileCountY)
+    {
+        List<Vector2Int> r = new List<Vector2Int>();
+        // Down
+        for (int i = currentY - 1; i >= 0; i--)
+        {
+            if (board [currentX, i] == null)
+                r.Add(new Vector2Int(currentX, i));
+
+            if (board [currentX, i] != null)
+            {
+                if(board [currentX, i].team != team)
+                r.Add(new Vector2Int (currentX, i));
+                break;
+            }
+        }
+
+        // Up
+        for (int i = currentY + 1; i < tileCountY; i++)
+        {
+            if (board [currentX, i] == null)
+                r.Add(new Vector2Int(currentX, i));
+
+            if (board [currentX, i] != null)
+            {
+                if(board [currentX, i].team != team)
+                r.Add(new Vector2Int (currentX, i));
+                break;
+            }
+        }
+
+        // Left
+        for (int i = currentX - 1; i >= 0; i--)
+        {
+            if (board [i, currentY] == null)
+                r.Add(new Vector2Int(i, currentY));
+
+            if (board [i, currentY] != null)
+            {
+                if(board [i, currentY].team != team)
+                r.Add(new Vector2Int (i, currentY));
+                break;
+            }
+        }
+
+        // Right
+        for (int i = currentX + 1; i < tileCountX; i++)
+        {
+            if (board [i, currentY] == null)
+                r.Add(new Vector2Int(i, currentY));
+
+            if (board [i, currentY] != null)
+            {
+                if(board [i, currentY].team != team)
+                r.Add(new Vector2Int (i, currentY));
+                break;
+            }
+        }
+        
+        return r;
+    }
     void Start()
     {
         
@@ -15,4 +76,5 @@ public class Rook : ChessPiece
     {
        
     }
+
 }
